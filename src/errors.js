094d64b96920715b300eee1036442faf9e0c92ca
Evgeny Razgovorov <razgovorov87@gmail.com>
2020-12-24 15:03:21 +0700

    add errors

--- src/errors.js
+++ src/errors.js
@@ -1,5 +1,6 @@
 export default {
     'auth/user-not-found': 'Пользователь с таким Email не найден',
     'auth/wrong-password': 'Неверный пароль!',
+    'auth/invalid-email': 'Неверный Email!',
     'auth/email-already-in-use': 'Пользователь с таким Email уже сущетсвует'
 }
